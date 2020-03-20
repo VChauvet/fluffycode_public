83e3758b8093cb654aa5f0411196e3f88758fc12
Valentin Chauvet <valentin.chauvet+git@gmail.com>
2020-03-20 19:11:55 +0000

    test

--- index.php
+++ index.php
@@ -8,7 +8,35 @@
 
 <body>
 	<h1>Most things start as chaos and finally become a orderly mess.</h1>
-	<p>GITGUD</p>
+	<p><?php
+			
+			        // create curl resource
+			$ch = curl_init();
+			$url = "https://corona.lmao.ninja/all";
+			//$url = "https://api.twitter.com/1/help/configuration.json";
+
+			// set url
+			curl_setopt($ch, CURLOPT_URL, $url);
+
+			//return the transfer as a string
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+			// $output contains the output string
+			$output = curl_exec($ch);
+
+
+			// close curl resource to free up system resources
+			curl_close($ch);
+			print $output;
+
+			if ($output === false){
+				echo "nope";
+			} else if ($output === true){
+				echo "yaza";
+			}
+
+		?>
+	</p>
 </div>
 
 </body>
