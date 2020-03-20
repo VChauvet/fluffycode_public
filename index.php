94b2c159fd1d784af73618c93ae706cb5b1671d4
Valentin Chauvet <valentin.chauvet+git@gmail.com>
2020-03-20 13:56:29 +0000

    added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<head>
+<meta http-equiv="Content-Language" content="de">
+<meta http-equiv="Content-Type" content="text/html; charset=WINDOWS-1252">
+<title>fluffycode</title>                      
+<style>
+</style>
+</head>
+
+<body>
+	<h1>Most things start as chaos and finally become a orderly mess.</h1>
+	<p>GITGUD</p>
+</div>
+
+</body>
+
+</html>
+
