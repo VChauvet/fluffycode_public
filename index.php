572d9ff5b43102a7411753f9dde8a7d3cb4dda63
Valentin Chauvet <valentin.chauvet+git@gmail.com>
2020-05-14 17:44:14 +0200

    removal of curl

--- index.php
+++ index.php
@@ -8,32 +8,7 @@
 
 <body>
 	<h1>Most things start as chaos and finally become a orderly mess.</h1>
-	<p><?php
-			
-			// create curl resource
-			$ch = curl_init();
-			$url = "https://corona.lmao.ninja/countries";
-			//$url = "https://api.twitter.com/1/help/configuration.json";
-
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-			$output = curl_exec($ch);
-
-
-			// close curl resource to free up system resources
-			curl_close($ch);
-
-			$arr_result = json_decode($output, true);
-
-			echo "<p>Germany cases: ".$arr_result[3]["cases"].", Deaths today: ".$arr_result[3]["todayDeaths"]."</p>";
-			echo "<p>Belgium cases: ".$arr_result[12]["cases"].", Deaths today: ".$arr_result[12]["todayDeaths"]."</p>";
-			echo "<p>Portugal cases: ".$arr_result[17]["cases"].", Deaths today: ".$arr_result[17]["todayDeaths"]."</p>";
-			
-			
-
-		?>
-	</p>
+	<p>Here we go again.</p>
 </div>
 
 </body>
